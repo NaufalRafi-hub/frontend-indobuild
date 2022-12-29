e404bb637def201af72cb2483177275ac3aa7e6c
naufalrafi-hub <rafi.harahaps@gmail.com>
2022-12-30 00:09:09 +0700

    add nav progress

--- src/Component/Nav/index.js
+++ src/Component/Nav/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import './nav.css'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Container } from 'react-bootstrap'
+import Logo from '../../Image/logo_event.png'
 
-const Nav = () => {
+const Navs = () => {
   return (
-    <div>Nav</div>
+    <div>
+        <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={Logo}></img>
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Explore Event</Nav.Link>
+            <Nav.Link href="#features">Help</Nav.Link>
+            {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
   )
 }
 
-export default Nav
+export default Navs
