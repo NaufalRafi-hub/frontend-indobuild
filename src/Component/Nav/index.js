1c7a6888c7cca8edcce500c9ba84324746ba4201
naufalrafi-hub <rafi.harahaps@gmail.com>
2022-12-30 03:37:05 +0700

    navs design

--- src/Component/Nav/index.js
+++ src/Component/Nav/index.js
@@ -2,24 +2,46 @@ import React from 'react'
 import './nav.css'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Container } from 'react-bootstrap'
+import { Button, Container } from 'react-bootstrap'
 import Logo from '../../Image/logo_event.png'
+import Search from '../../Image/Shape.png'
 
 const Navs = () => {
   return (
     <div>
-        <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img src={Logo}></img>
-          </Navbar.Brand>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="#home"><img src={Logo} width='122' ></img></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Explore Event</Nav.Link>
-            <Nav.Link href="#features">Help</Nav.Link>
-            {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+            {/* <Nav.Link href="#features"> */}
+            <div className=' wrap-search d-flex align-items-center'>
+              <img className='imgSearch'src={Search}></img>
+              <input className='search-bar'type="text" placeholder="Search your venue location"></input>
+            </div>
+            {/* </Nav.Link> */}
+            <div className='wrap-exp d-flex'>
+              <Nav.Link href="#pricing">Explore Event</Nav.Link>
+              <Nav.Link href="#">Help</Nav.Link>
+              <div className='line'></div>
+            </div>
+            
           </Nav>
-        </Container>
-      </Navbar>
+          <Nav>
+          {/* <Nav.Link href="#pricing">Explore Event</Nav.Link>
+            <Nav.Link href="#">Help</Nav.Link>
+            <div className='line'></div> */}
+            <Nav.Link href="#deets">
+              <Button className='btn-event'>Create Event</Button>
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              <Button className='btn-sign'>Sign In</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </div>
   )
 }
