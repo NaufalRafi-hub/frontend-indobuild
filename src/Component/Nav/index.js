83ccad7973184e156e1d14a47ef00615a19c92eb
naufalrafi-hub <rafi.harahaps@gmail.com>
2022-12-30 03:50:12 +0700

    fix navs layout

--- src/Component/Nav/index.js
+++ src/Component/Nav/index.js
@@ -10,7 +10,7 @@ const Navs = () => {
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-      <Container>
+      <Container fluid>
         <Navbar.Brand href="#home"><img src={Logo} width='122' ></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -21,17 +21,12 @@ const Navs = () => {
               <input className='search-bar'type="text" placeholder="Search your venue location"></input>
             </div>
             {/* </Nav.Link> */}
-            <div className='wrap-exp d-flex'>
-              <Nav.Link href="#pricing">Explore Event</Nav.Link>
-              <Nav.Link href="#">Help</Nav.Link>
-              <div className='line'></div>
-            </div>
             
           </Nav>
-          <Nav>
-          {/* <Nav.Link href="#pricing">Explore Event</Nav.Link>
-            <Nav.Link href="#">Help</Nav.Link>
-            <div className='line'></div> */}
+          <Nav className='ms-auto'>
+          <Nav.Link className='mr-30 d-flex align-items-center' href="#pricing">Explore Event</Nav.Link>
+            <Nav.Link className='mr-30 d-flex align-items-center' href="#">Help</Nav.Link>
+            <div className='line mr-30'></div>
             <Nav.Link href="#deets">
               <Button className='btn-event'>Create Event</Button>
             </Nav.Link>
